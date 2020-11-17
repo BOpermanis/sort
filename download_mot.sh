f1911e44d7607103f840546d1da814a3e82dd9ab
Bruno <bruno.opermanis@gmail.com>
2020-11-17 17:24:26 +0200

    for workshop

---
+++ download_mot.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget https://motchallenge.net/data/2DMOT2015.zip
+unzip 2DMOT2015.zip
--- sort_workshop.py
+++ sort_workshop.py
@@ -33,16 +33,18 @@ from filterpy.kalman import KalmanFilter
 
 np.random.seed(0)
 
-
-def linear_assignment(cost_matrix):
-    try:
-        import lap
+try:
+    import lap
+    def linear_assignment(cost_matrix):
         _, x, y = lap.lapjv(cost_matrix, extend_cost=True)
         return np.array([[y[i], i] for i in x if i >= 0])  #
-    except ImportError:
-        from scipy.optimize import linear_sum_assignment
+
+except ImportError:
+    from scipy.optimize import linear_sum_assignment
+    def linear_assignment(cost_matrix):
         x, y = linear_sum_assignment(cost_matrix)
         return np.array(list(zip(x, y)))
+
 
 
 def iou_batch(bb_test, bb_gt):
@@ -86,7 +88,7 @@ def convert_x_to_bbox(x, score=None):
     """
     w = np.sqrt(x[2] * x[3])
     h = x[2] / w
-    if (score == None):
+    if score is None:
         return np.array([x[0] - w / 2., x[1] - h / 2., x[0] + w / 2., x[1] + h / 2.]).reshape((1, 4))
     else:
         return np.array([x[0] - w / 2., x[1] - h / 2., x[0] + w / 2., x[1] + h / 2., score]).reshape((1, 5))
@@ -105,10 +107,19 @@ class KalmanBoxTracker(object):
         # define constant velocity model
         self.kf = KalmanFilter(dim_x=7, dim_z=4)
         self.kf.F = np.array(
-            [[1, 0, 0, 0, 1, 0, 0], [0, 1, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0, 1], [0, 0, 0, 1, 0, 0, 0],
-             [0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 1]])
+            [[1, 0, 0, 0, 1, 0, 0],
+             [0, 1, 0, 0, 0, 1, 0],
+             [0, 0, 1, 0, 0, 0, 1],
+             [0, 0, 0, 1, 0, 0, 0],
+             [0, 0, 0, 0, 1, 0, 0],
+             [0, 0, 0, 0, 0, 1, 0],
+             [0, 0, 0, 0, 0, 0, 1]])
+
         self.kf.H = np.array(
-            [[1, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]])
+            [[1, 0, 0, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0],
+             [0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 1, 0, 0, 0]])
 
         self.kf.R[2:, 2:] *= 10.
         self.kf.P[4:, 4:] *= 1000.  # give high uncertainty to the unobservable initial velocities
@@ -139,12 +150,15 @@ class KalmanBoxTracker(object):
         """
         Advances the state vector and returns the predicted bounding box estimate.
         """
-        if ((self.kf.x[6] + self.kf.x[2]) <= 0):
+        if self.kf.x[6] + self.kf.x[2] <= 0:
             self.kf.x[6] *= 0.0
+
         self.kf.predict()
         self.age += 1
-        if (self.time_since_update > 0):
+
+        if self.time_since_update > 0:
             self.hit_streak = 0
+
         self.time_since_update += 1
         self.history.append(convert_x_to_bbox(self.kf.x))
         return self.history[-1]
@@ -165,38 +179,10 @@ def associate_detections_to_trackers(detections, trackers, iou_threshold=0.3):
     if (len(trackers) == 0):
         return np.empty((0, 2), dtype=int), np.arange(len(detections)), np.empty((0, 5), dtype=int)
 
-    iou_matrix = iou_batch(detections, trackers)
 
-    if min(iou_matrix.shape) > 0:
-        a = (iou_matrix > iou_threshold).astype(np.int32)
-        if a.sum(1).max() == 1 and a.sum(0).max() == 1:
-            matched_indices = np.stack(np.where(a), axis=1)
-        else:
-            matched_indices = linear_assignment(-iou_matrix)
-    else:
-        matched_indices = np.empty(shape=(0, 2))
+    raise NotImplemented
 
-    unmatched_detections = []
-    for d, det in enumerate(detections):
-        if (d not in matched_indices[:, 0]):
-            unmatched_detections.append(d)
-    unmatched_trackers = []
-    for t, trk in enumerate(trackers):
-        if (t not in matched_indices[:, 1]):
-            unmatched_trackers.append(t)
-
-    # filter out matched with low IOU
-    matches = []
-    for m in matched_indices:
-        if (iou_matrix[m[0], m[1]] < iou_threshold):
-            unmatched_detections.append(m[0])
-            unmatched_trackers.append(m[1])
-        else:
-            matches.append(m.reshape(1, 2))
-    if (len(matches) == 0):
-        matches = np.empty((0, 2), dtype=int)
-    else:
-        matches = np.concatenate(matches, axis=0)
+    # TODO implemeteet associaaciju izveidoshanu
 
     return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
 
@@ -231,9 +217,11 @@ class Sort(object):
             trk[:] = [pos[0], pos[1], pos[2], pos[3], 0]
             if np.any(np.isnan(pos)):
                 to_del.append(t)
+
         trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
         for t in reversed(to_del):
             self.trackers.pop(t)
+
         matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(dets, trks, self.iou_threshold)
 
         # update matched trackers with assigned detections
@@ -244,16 +232,20 @@ class Sort(object):
         for i in unmatched_dets:
             trk = KalmanBoxTracker(dets[i, :])
             self.trackers.append(trk)
+
         i = len(self.trackers)
         for trk in reversed(self.trackers):
             d = trk.get_state()[0]
+
             if (trk.time_since_update < 1) and (trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits):
                 ret.append(np.concatenate((d, [trk.id + 1])).reshape(1, -1))  # +1 as MOT benchmark requires positive
             i -= 1
+
             # remove dead tracklet
-            if (trk.time_since_update > self.max_age):
+            if trk.time_since_update > self.max_age:
                 self.trackers.pop(i)
-        if (len(ret) > 0):
+
+        if len(ret) > 0:
             return np.concatenate(ret)
         return np.empty((0, 5))
 
